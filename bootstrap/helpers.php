36e6069e82d8415a3462616e50450d6615808b21
Sian Chou <webcodedemo@gmail.com>
2019-03-09 11:07:51 +0000

    基礎頁面

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,2 +1,7 @@
 <?php
 
+// 此方法會將當前請求的路由名稱轉換為CSS Class名稱，作用是允許我們針對某個頁面做頁面樣式訂製
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Sian Chou 2019
+    </p>
+    <p class="float-right"><a href="mailto:webcodedemo@gmail.com">聯絡我們</a></p>
+  </div>
+</footer>
