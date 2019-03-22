86a3bdcf1ca2550d864826fcfe0644e714e7e2aa
Sian Chou <webcodedemo@gmail.com>
2019-03-22 02:09:08 +0000

    註冊與登入

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,24 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="#">登入</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">註冊</a></li>
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="logout" href="#"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">登出</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
