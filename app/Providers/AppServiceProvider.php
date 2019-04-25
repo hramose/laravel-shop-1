b6c0adba68df8d9eaeaa5431f6083befa58ef2c5
Sian Chou <webcodedemo@gmail.com>
2019-04-25 01:47:33 +0000

    安裝支付套件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,41 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        // 往服務容器中注入一個名為 alipay 的單例物件
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 判斷當前專案運行環境是否為線上環境
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 調用 Yansongda\Pay 來建立一個支付寶支付物件
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 調用 Yansongda\Pay 來建立一個微信支付物件
+            return Pay::wechat($config);
+        });
     }
 }
